3a228644fb9225d62295289627e19d8925b94500
hootis <81767621+Valshion007@users.noreply.github.com>
2023-03-21 09:38:59 -0700

    another attempt at flight

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using SlugBase.Features;
 using static SlugBase.Features.FeatureTypes;
 using MoreSlugcats;
+using IL.RWCustom;
+using RWCustom;
+using System.Diagnostics.Eventing.Reader;
 
 namespace SlugTemplate
 {
@@ -11,7 +14,9 @@ namespace SlugTemplate
     class Plugin : BaseUnityPlugin
     {
         private const string MOD_ID = "aeriform";
-        private bool inAir = false;
+        private float airTime = 0f;
+        //private bool grabbingPole = false;
+        private bool canFly = false;
 
         // Add hooks
         public void OnEnable()
@@ -22,6 +27,8 @@ namespace SlugTemplate
             //On.Player.Jump += PlayerJumpHook;
             On.Player.Update += PlayerUpdateHook;
             On.Player.Die += PlayerDieHook;
+            On.Player.MovementUpdate += MovementHook;
+            On.Player.TerrainImpact += LevelCollisionHook;
         }
         
         // Load any resources, such as sprites or sounds
@@ -34,16 +41,38 @@ namespace SlugTemplate
         private void PlayerUpdateHook(On.Player.orig_Update orig, Player self, bool eu)
         {
             orig(self, eu);
-
+            
             if (self.input[0].y > 0)
             {
-                self.Jump();
+                if (canFly)
+                {
+                    self.Jump();
+                    airTime++;
+                }
             }
+
+            if (airTime > 200)
+            {
+                canFly = false;
+            }
+        }
+
+        private void LevelCollisionHook(On.Player.orig_TerrainImpact orig, Player self, int chunk, RWCustom.IntVector2 direction, float speed, bool firstContact)
+        {
+            orig(self, chunk, direction, speed, firstContact);
+            canFly = true;
         }
 
         private void PlayerDieHook(On.Player.orig_Die orig, Player self)
         {
             orig(self);
+            //wait what was i using this for?
+        }
+
+        private void MovementHook(On.Player.orig_MovementUpdate orig, Player self, bool eu)
+        {
+            orig(self, eu);
+            //uhh do something with self.GrabVerticalPole or something
         }
 
         // Implement Flight (old method)
